04556ad9b2256a5449f8362831f61a00be27cd9a
NickJonesGitty <nicholas.jones@mymail.champlain.edu>
2022-02-16 14:36:21 -0500

    SSH File

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author NickJonesGitty
+#creates a new ssh user using $l parameter
+#adds a public key from the local repo or curled from the remote
+#removes roots ability to ssh in
+echo "ALL CODE HERE"
